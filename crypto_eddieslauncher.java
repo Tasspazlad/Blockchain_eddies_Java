b4ade459db7f9d47df53c959b3637ca3229e7e04
HarleyTaskis <harley.taskis@gmail.com>
2021-08-04 22:06:00 +1000

    launcher

---
+++ crypto_eddieslauncher.java
@@ -0,0 +1,8 @@
+public class crypto_eddieslauncher {
+    private crypto_eddieslauncher() {
+    }
+    public static void main(String[] args) {
+        crypto_eddies register = new crypto_eddies();
+        register.run(args);
+    }  
+}
