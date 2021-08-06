8ea656b6a6d9a0c0c34cb1904c1c7f39483d4ebf
HarleyTaskis <harley.taskis@gmail.com>
2021-08-06 13:50:40 +1000

    transaction +

--- crypto_eddies/Transaction.java
+++ crypto_eddies/Transaction.java
@@ -37,7 +37,11 @@ public class Transaction {
         String data = StringUtil.getStringFromKey(sender) +  StringUtil.getStringFromKey(reciepient) + Float.toString(value)    ;
         signature = StringUtil.applyECDSASig(privateKey, data);
     }
+    //Verifies the data we signed hasnt been tampered with
+    public boolean verifiySignature() {
+        String data = StringUtil.getStringFromKey(sender) + StringUtil.getStringFromKey(reciepient) + Float.toString(value)     ;
+        return StringUtil.verifyECDSASig(sender, data, signature);
+    }
 
-    
     
 }
