e27c6e6c900e873eb868edcb4124eac5ce57bdea
HarleyTaskis <harley.taskis@gmail.com>
2021-08-06 10:16:47 +1000

    wallets

---
+++ crypto_eddies/Wallet.java
@@ -0,0 +1,28 @@
+package crypto_eddies;
+import java.security.*;
+import java.security.spec.ECGenParameterSpec;
+
+public class Wallet {
+    public PrivateKey privateKey;
+    public PublicKey publicKey;
+
+    public Wallet(){
+        generateKeyPair();
+    }
+
+    public void generateKeyPair() {
+        try {
+            KeyPairGenerator keyGen = KeyPairGenerator.getInstance("ECDSA","BC");
+            SecureRandom random = SecureRandom.getInstance("SHA1PRNG");
+            ECGenParameterSpec ecSpec = new ECGenParameterSpec("prime192v1");
+            // Initialize the key generator and generate a keyPair
+            keyGen.initialize(ecSpec, random);  //256 bytes provides an acceptable security level
+            KeyPair keyPair = keyGen.generateKeyPair();
+            // set the public and private keys from the keyPair
+            privateKey = keyPair.getPrivate();
+            publicKey = keyPair.getPublic();
+        }catch(Exception e) {
+            throw new RuntimeException();
+        }
+    }
+}
