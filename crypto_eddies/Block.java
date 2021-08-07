7f671a36871a7e5fd8afb263fed9237622d94e87
HarleyTaskis <harley.taskis@gmail.com>
2021-08-07 22:21:44 +1000

    block mod

--- crypto_eddies/Block.java
+++ crypto_eddies/Block.java
@@ -1,18 +1,18 @@
 package crypto_eddies;
-
+import java.util.ArrayList;
 import java.util.Date;
 
 public class Block {
 
     public String hash;
     public String previousHash;
-    private String data; //for demo purpose this will be a simple message.
+    public String merkleRoot;
+    public ArrayList<Transaction> transactions = new ArrayList<Transaction>(); //our data will be a simple message.
     private long timeStamp; //as number of milliseconds since 1/1/1970.
     private int nonce;
 
     //Block Constructor.
     public Block(String data,String previousHash ) {
-        this.data = data;
         this.previousHash = previousHash;
         this.timeStamp = new Date().getTime();
         
@@ -26,18 +26,23 @@ public class Block {
             previousHash +
             Long.toString(timeStamp) +
             Integer.toString(nonce) +
-            data
+            merkleRoot
         );
         return calculatedhash;
     }
 
+    //Increases nonce value until hash target is reached.
     public void mineBlock(int difficulty) {
-        String target = new String(new char[difficulty]).replace('\0', '0'); //Create a string with difficulty * "0"
+        merkleRoot = StringUtil.getMerkleRoot(transactions);
+        String target = StringUtil.getDificultyString(difficulty); //Create a string with difficulty * "0"
         while(!hash.substring( 0, difficulty).equals(target)) {
             nonce ++;
             hash = calculateHash();
         }
         System.out.println("Block Mined!!! : " + hash);
     }
+
+
+    //Add transactions to this block
     
 }
